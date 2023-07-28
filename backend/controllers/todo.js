692fa732d796d8cecc649dba1318fa7436338c29
Salih Alperen <sal.alperen1@gmail.com>
2023-07-29 00:58:58 +0200

    Add first version of controllers

--- backend/controllers/todo.js
+++ backend/controllers/todo.js
@@ -1,16 +1,43 @@
+import { Todo } from "../models/Todo.js";
+
 const getTodos = async (req, res) => {
-  res.status(200).send("Here already");
+  const todos = await Todo.find({});
+  res.status(200).send({ todos, msg: "Success" });
 };
 
-const addTodo = async (req, res) => {};
+const addTodo = async (req, res) => {
+  const { todo } = req.body;
+  if (!todo) {
+    throw new Error("Please provide the values");
+  }
+  const newTodo = await Todo.create({ todo });
+  res.status(200).json({ todo: newTodo });
+};
 
 const getSingleTodo = async (req, res) => {
   const { id } = req.params;
   res.status(200).send("single todo");
 };
 
-const updateTodo = async (req, res) => {};
+const updateTodo = async (req, res) => {
+  const { id } = req.params;
+  const { todo, isCompleted } = req.body;
+  const updatedTodo = await Todo.findOneAndUpdate(
+    { _id: id },
+    { todo, isCompleted },
+    {
+      new: true,
+      overwrite: true,
+    }
+  );
+  res.send({ todo: updatedTodo });
+};
 
-const deleteTodo = async (req, res) => {};
+const deleteTodo = async (req, res) => {
+  const { id } = req.params;
+  const deletedTodo = await Todo.findOneAndDelete({ _id: id });
+  console.log(deletedTodo);
+  res.send("Generic message!");
+};
 
 export { getTodos, addTodo, getSingleTodo, updateTodo, deleteTodo };
