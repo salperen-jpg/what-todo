f17bedb516df85df8e9a4fb65a8d03b28f901d0f
Salih Alperen <sal.alperen1@gmail.com>
2023-07-29 23:37:37 +0200

    Add more errors

--- backend/errors/CustomError.js
+++ backend/errors/CustomError.js
@@ -1,7 +1,37 @@
+import { StatusCodes } from "http-status-codes";
+
 class CustomError extends Error {
-  constructor(msg) {
-    super(msg);
+  constructor(message) {
+    super(message);
   }
 }
 
-export default CustomError;
+class NotFound extends CustomError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.NOT_FOUND;
+  }
+}
+
+class BadRequest extends CustomError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+class Unauthenticated extends CustomError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.UNAUTHORIZED;
+  }
+}
+
+class Unauthorized extends CustomError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.FORBIDDEN;
+  }
+}
+
+export { CustomError, NotFound, BadRequest, Unauthorized, Unauthenticated };
