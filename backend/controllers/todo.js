b144ff1d5dc3f55a17df8bf2d7ef9fbe9ff2f8cf
Salih Alperen <sal.alperen1@gmail.com>
2023-07-29 23:38:31 +0200

    Add statuscodes and erros to controller

--- backend/controllers/todo.js
+++ backend/controllers/todo.js
@@ -1,43 +1,58 @@
-import { Todo } from "../models/Todo.js";
-
+import Todo from "../models/Todo.js";
+import { BadRequest, NotFound } from "../errors/CustomError.js";
+import { StatusCodes } from "http-status-codes";
 const getTodos = async (req, res) => {
   const todos = await Todo.find({});
-  res.status(200).send({ todos, msg: "Success" });
+  res.status(StatusCodes.OK).json({ todos, msg: "Success" });
 };
 
 const addTodo = async (req, res) => {
   const { todo } = req.body;
   if (!todo) {
-    throw new Error("Please provide the values");
+    throw new BadRequest("Please provide the values");
   }
   const newTodo = await Todo.create({ todo });
-  res.status(200).json({ todo: newTodo });
+  res.status(StatusCodes.CREATED).json({ todo: newTodo });
 };
 
 const getSingleTodo = async (req, res) => {
   const { id } = req.params;
-  res.status(200).send("single todo");
+  const todo = await Todo.findById(id);
+  if (!todo) throw new NotFound(`No todo with the id : ${id}`);
+  res.status(StatusCodes.OK).json({ todo });
 };
 
 const updateTodo = async (req, res) => {
   const { id } = req.params;
   const { todo, isCompleted } = req.body;
-  const updatedTodo = await Todo.findOneAndUpdate(
-    { _id: id },
+  const updatedTodo = await Todo.findByIdAndUpdate(
+    id,
     { todo, isCompleted },
     {
       new: true,
       overwrite: true,
     }
   );
+  if (!updateTodo) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `No todo with the id : ${id}` });
+  }
   res.send({ todo: updatedTodo });
 };
 
 const deleteTodo = async (req, res) => {
   const { id } = req.params;
-  const deletedTodo = await Todo.findOneAndDelete({ _id: id });
-  console.log(deletedTodo);
-  res.send("Generic message!");
+  const deletedTodo = await Todo.findByIdAndDelete(id);
+  if (!deletedTodo) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `No todo with the id : ${id}` });
+  }
+  res.status(200).json({
+    todo: deletedTodo,
+    msg: "succes",
+  });
 };
 
 export { getTodos, addTodo, getSingleTodo, updateTodo, deleteTodo };
