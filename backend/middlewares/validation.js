37e459a7076524d0413ed1e618ee4c602c462244
Salih Alperen <sal.alperen1@gmail.com>
2023-08-02 00:28:22 +0200

    Add additional valdiation

--- backend/middlewares/validation.js
+++ backend/middlewares/validation.js
@@ -1,6 +1,7 @@
 import { param, query, body, validationResult } from "express-validator";
-import { BadRequest } from "../errors/CustomError.js";
+import { BadRequest, NotFound } from "../errors/CustomError.js";
 import mongoose from "mongoose";
+import Todo from "../models/Todo.js";
 
 const validationWithErrorHandler = (chainValues) => {
   return [
@@ -9,6 +10,9 @@ const validationWithErrorHandler = (chainValues) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         const errMessages = errors.array().map((err) => err.msg);
+        if (errMessages[0].startsWith("Todo")) {
+          throw new NotFound(errMessages);
+        }
         throw new BadRequest(errMessages);
       }
       next();
@@ -16,23 +20,26 @@ const validationWithErrorHandler = (chainValues) => {
   ];
 };
 
-const todoChain = () =>
+// todo validation
+const todoChain = () => [
   body("todo")
     .notEmpty()
     .withMessage("Please provide the todo!")
     .isLength({ min: 3, max: 100 })
-    .withMessage("Todo must be between 3 char and 100 char!");
+    .withMessage("Todo must be between 3 char and 100 char!"),
+  body("isCompleted").notEmpty().withMessage("Please provide the completed!"),
+];
 
 export const todoValidation = validationWithErrorHandler(todoChain());
 
 // id validation
-
 const idParamChain = () =>
-  param("id")
-    .custom((value) => {
-      console.log(value);
-      return mongoose.Types.ObjectId.isValid(value);
-    })
-    .withMessage("Unmatched id param");
+  param("id").custom(async (value) => {
+    const isValidId = mongoose.Types.ObjectId.isValid(value);
+    if (!isValidId) throw new BadRequest("Invalid id parameter");
+    const specificTodo = await Todo.findById(value);
+    if (!specificTodo)
+      throw new NotFound(`Todo is not found with the id : ${value}`);
+  });
 
 export const idValidation = validationWithErrorHandler(idParamChain());
