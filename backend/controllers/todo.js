5bdae70d471e89515e4629b98f7a59f477de5d84
Salih Alperen <sal.alperen1@gmail.com>
2023-08-13 23:38:04 +0200

    Add relation user and todo

--- backend/controllers/todo.js
+++ backend/controllers/todo.js
@@ -2,16 +2,17 @@ import Todo from "../models/Todo.js";
 import { BadRequest, NotFound } from "../errors/CustomError.js";
 import { StatusCodes } from "http-status-codes";
 const getTodos = async (req, res) => {
-  const todos = await Todo.find({});
+  const todos = await Todo.find({ createdBy: req.user.userId });
   res.status(StatusCodes.OK).json({ todos, msg: "Success" });
 };
 
 const addTodo = async (req, res) => {
   const { todo, isCompleted } = req.body;
+  const { userId, role } = req.user;
   if (!todo) {
     throw new BadRequest("Please provide the values");
   }
-  const newTodo = await Todo.create({ todo, isCompleted });
+  const newTodo = await Todo.create({ todo, isCompleted, createdBy: userId });
   res.status(StatusCodes.CREATED).json({ todo: newTodo });
 };
 
